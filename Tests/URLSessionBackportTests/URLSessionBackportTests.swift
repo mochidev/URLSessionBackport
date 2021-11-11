9d94cb730e2bf80c079c6f2c59b5804946917dea
Dimitri Bouniol <dimitribouniol@mochidev.com>
2021-11-11 12:50:58 -0800

    Replaced preconditionFailure with a thrown error to prevent crashes in production code

--- Tests/URLSessionBackportTests/URLSessionBackportTests.swift
+++ Tests/URLSessionBackportTests/URLSessionBackportTests.swift
@@ -13,4 +13,85 @@ final class URLSessionBackportTests: XCTestCase {
             XCTAssertTrue(backportedURLSession.delegate is SessionDelegateProxy)
         }
     }
+    
+    func testContinuationTaskCompletionHandler() async throws {
+        enum DummyError: Error {
+            case error
+        }
+        
+        do {
+            let _: (String, String) = try await withUnsafeThrowingContinuation { continuation in
+                continuation.taskCompletionHandler("A", nil, nil)
+            }
+            XCTFail()
+        } catch URLSession.Backport.Error.unexpectedTaskCompletionHandler(true, false, false) {
+            // Got the expected case!
+        } catch {
+            XCTFail()
+        }
+        
+        do {
+            let _: (String, String) = try await withUnsafeThrowingContinuation { continuation in
+                continuation.taskCompletionHandler(nil, "B", nil)
+            }
+            XCTFail()
+        } catch URLSession.Backport.Error.unexpectedTaskCompletionHandler(false, true, false) {
+            // Got the expected case!
+        } catch {
+            XCTFail()
+        }
+        
+        do {
+            let _: (String, String) = try await withUnsafeThrowingContinuation { continuation in
+                continuation.taskCompletionHandler(nil, nil, DummyError.error)
+            }
+            XCTFail()
+        } catch DummyError.error {
+            // Got the expected case!
+        } catch {
+            XCTFail()
+        }
+        
+        do {
+            let result: (String, String) = try await withUnsafeThrowingContinuation { continuation in
+                continuation.taskCompletionHandler("A", "B", nil)
+            }
+            XCTAssert(result == ("A", "B"))
+        } catch {
+            XCTFail()
+        }
+        
+        do {
+            let _: (String, String) = try await withUnsafeThrowingContinuation { continuation in
+                continuation.taskCompletionHandler("A", nil, DummyError.error)
+            }
+            XCTFail()
+        } catch URLSession.Backport.Error.unexpectedTaskCompletionHandler(true, false, true) {
+            // Got the expected case!
+        } catch {
+            XCTFail()
+        }
+        
+        do {
+            let _: (String, String) = try await withUnsafeThrowingContinuation { continuation in
+                continuation.taskCompletionHandler(nil, "B", DummyError.error)
+            }
+            XCTFail()
+        } catch URLSession.Backport.Error.unexpectedTaskCompletionHandler(false, true, true) {
+            // Got the expected case!
+        } catch {
+            XCTFail()
+        }
+        
+        do {
+            let _: (String, String) = try await withUnsafeThrowingContinuation { continuation in
+                continuation.taskCompletionHandler("A", "B", DummyError.error)
+            }
+            XCTFail()
+        } catch URLSession.Backport.Error.unexpectedTaskCompletionHandler(true, true, true) {
+            // Got the expected case!
+        } catch {
+            XCTFail()
+        }
+    }
 }
